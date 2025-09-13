222aaeaac1b783c76787648ea7e4fd1ff665ce05
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2025-09-13 10:31:09 +0100

    Create data_classification.py

---
+++ data_classification.py
@@ -0,0 +1,5 @@
+# 1- structure typ:structured, value: Qualitative, source:secondary
+# 2 - structure typ:semi-structured, value: Qualitative, source:primary
+# 3 - structure typ:structured, value: Qualitative, source:primary
+# 4 - structure typ:structured, value: quantitative, source:secondary
+# 5 - structure typ:semi-structured, value: Quantitative, source:primary
